331d5e00690d4760f3a0ad95c4c0db92e5bb5fbd
Bhavya Barai <94623439+Bhavya2004@users.noreply.github.com>
2023-05-16 12:33:55 +0530

    Add files via upload

---
+++ Javascript/Promise_Time_Limit.js
@@ -0,0 +1,18 @@
+var timeLimit = function(fn, t) {
+    return async function(...args) {
+        return new Promise((resolve, reject) => { // create a new promise to return 
+
+        // create a timer to reject the promise if the `t` time passes
+        const timeout = setTimeout(() => {
+            reject("Time Limit Exceeded");
+        }, t);
+
+        // start using the function as usual if it's succed before just return it
+        fn(...args)
+            .then(resolve)
+            .catch(reject)
+            // clear timer is needed to free memory from setTimeout - good practise
+            .finally(() => clearTimeout(timeout));
+        })
+    }
+};
